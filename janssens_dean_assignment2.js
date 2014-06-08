ea18a7c7ebd0270c306e20d62584c01c4026dab4
Dean Janssens <djanssens@fullsail.edu>
2014-06-08 16:30:43 -0500

    Added structure based on flowchart

--- janssens_dean_assignment2.js
+++ janssens_dean_assignment2.js
@@ -1,6 +1,62 @@
 //alert("JavaScript works!");
-
 //Dean Janssens
 //SDI 1406
 //June 12, 2014
 //Project 2
+//Variables
+var = " ";
+var = ;
+var = ;
+
+//Prompt
+
+prompt()
+
+//Procedure
+
+
+var = function () {
+    var;
+    if () {} else {}
+};
+
+//Confirm
+
+confirm()
+
+
+//Boolean Function
+
+var getBoolean = function () {
+    var;
+    while () {}
+    return;
+
+};
+
+
+
+//Number Function
+
+var getNumber = function () {
+    var;
+    for () {}
+    return;
+};
+
+
+
+
+//String Function
+
+var getString = function () {
+    var;
+    if () {} else {}
+
+};
+
+//Returned Values
+
+getBoolean();
+getNumber();
+getString();
