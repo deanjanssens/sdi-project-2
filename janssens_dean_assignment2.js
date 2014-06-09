22a45a32f1e7c3dd80e76a253acb6040e40f9537
Dean Janssens <djanssens@fullsail.edu>
2014-06-09 16:47:41 -0500

    added procedure

--- janssens_dean_assignment2.js
+++ janssens_dean_assignment2.js
@@ -4,32 +4,51 @@
 //June 12, 2014
 //Project 2
 //Variables
-var = " ";
-var = ;
-var = ;
+var character1 = "Rick";
+var character2 = "Daryl";
+var character3 = "Michonne";
+var weaponOfChoice1 = ".357 Magnum";
+var weaponOfChoice2 = "Crossbow";
+var weaponOfChoice3 = "Katana";
+var zombiesPresent = true;
+var numberOfZombies = 3;
 
 //Prompt
 
-prompt()
+var characterChoice = prompt("Which character do you choose?", "Please choose Rick, Daryl or Michonne.");
 
 //Procedure
 
 
-var = function () {
-    var;
-    if () {} else {}
+var getCharacter = function () {
+    if (characterChoice === "Rick") {
+        var characterName = character1
+        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice1 + ".");
+    } else if (characterChoice === "Daryl") {
+        var characterName = character2
+        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice2 + ".");
+    } else if (characterChoice === "Michonne") {
+        var characterName = character3
+        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice3 + ".");
+    } else {
+    console.log("Please refresh and choose a character." + " " + characterChoice);
+    }
+    return;
+
 };
+
+getCharacter();
 
 //Confirm
 
-confirm()
+confirm("Your " + character + "is" + " and your weapon is " + weapon + ".")
 
 
 //Boolean Function
 
 var getBoolean = function () {
     var;
-    while () {}
+    while (zombiesPresent === true) {}
     return;
 
 };
@@ -38,8 +57,8 @@ var getBoolean = function () {
 
 //Number Function
 
-var getNumber = function () {
-    var;
+var getNumber = function (numberOfZombies) {
+    var ;
     for () {}
     return;
 };
