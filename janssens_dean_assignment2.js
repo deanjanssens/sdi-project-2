cadcf04f5628f6ba285247d346fc51a281d8a51f
Dean Janssens <djanssens@fullsail.edu>
2014-06-11 04:57:43 -0500

    last file did not save correctly this is updated file

--- janssens_dean_assignment2.js
+++ janssens_dean_assignment2.js
@@ -4,58 +4,42 @@
 //June 12, 2014
 //Project 2
 //Variables
-var character1 = "Rick";
-var character2 = "Daryl";
-var character3 = "Michonne";
-var weaponOfChoice1 = ".357 Magnum";
-var weaponOfChoice2 = "Crossbow";
-var weaponOfChoice3 = "Katana";
-var zombiesPresent = true;
+var character = "Daryl";
+var weapon2 = ".357 Magnum";
+var weapon1 = "Crossbow";
+var zombiesPresent;
 var numberOfZombies = 3;
+var numberOfSurvivors;
 
 //Prompt
 
-var characterChoice = prompt("Which character do you choose?", "Please choose Rick, Daryl or Michonne.");
+var weaponChoice = prompt("Which weapon will " + character + " use?", weapon1 + " or " + weapon2);
 
 //Procedure
 
-
-var getCharacter = function () {
-    if (characterChoice === "Rick") {
-        var characterName = character1
-        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice1 + ".");
-    } else if (characterChoice === "Daryl") {
-        var characterName = character2
-        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice2 + ".");
-    } else if (characterChoice === "Michonne") {
-        var characterName = character3
-        console.log("Your character is " + characterName + " and your weapon of choice is a " + weaponOfChoice3 + ".");
-    } else {
-    console.log("Please refresh and choose a character." + " " + characterChoice);
-    }
-    return;
-
+var getWeapon = function (weaponChoice, weapon1 ,weapon2){
+	if (weaponChoice === weapon1){
+	console.log("The " + weapon1 + " is " + character + "'s" + " weapon of choice.");
+	}else{ 
+	console.log(character + " can also use a " + weapon2 + ".");	
+	}
 };
 
-getCharacter();
+getWeapon(weaponChoice, weapon1, weapon2);
 
 //Confirm
 
-var zombiesPresent = confirm(Are there zombies present?)
-
+var zombiesPresent = confirm("Are zombies present?");
 
 //Boolean Function
 
-var getZombies = function () {
-    var zombies = 0;
-    while (zombies < 3) {
-    console.log("There are zombies present. There are more to remove."}
-    console.log("There are no zombies present.");
-    return zombies;
+var getzombiesPresent = function (){
+console.log ("It is " + zombiesPresent + " that zombies are present.");
 
 };
 
-getZombies();
+getzombiesPresent();
+
 
 
 //Number Function
